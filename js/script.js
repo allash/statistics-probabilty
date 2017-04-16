fcc8d36b164ebec5f8d0adf74a0f6941f2350aee
Allash <allashremoter@gmail.com>
2017-04-16 08:40:54 +0200

    updated js scripts, added new r files

--- js/script.js
+++ js/script.js
@@ -60,11 +60,18 @@ function median(data) {
   return median;
 }
 
+function sum(data) {
+  var list = data.split(' ').map(e => { return parseInt(e); });
+  return list.reduce((a, b) => a + b, 0);
+}
+
 
 var data = '34 34 27 37 42 41 36 32 41 33 31 74 33 49 38 61 21 41 26 80 42 29 33 36 45 49 39 34 26 25 33 35 35 28 30 29 61 32 33 45 29 62 22 44';
 var data2 = '1 6 7 5 8 5 11 12 15';
-stemplot(data2);
-document.write('median: ' + median(data));
+var patientReadingLevel = '6 4 4 3 3 2 6 5 4 7 2 17';
+var pamphletReadingLevel = '3 3 8 4 1 1 4 2 1 2 1';
+var stemplot = stemplot(pamphletReadingLevel);
 
-0 | 155678
-1 | 125
+document.write('<br />');
+document.write('median: ' + median(pamphletReadingLevel));
+document.write('sum: ' + sum(pamphletReadingLevel));
